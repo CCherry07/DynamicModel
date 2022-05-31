e4434a9ed4dbe151bf85d85ec01ed72be4883de2
2405693142@qq.com <2405693142@qq.com>
2022-05-31 11:32:03 +0800

    feat:  confirmDeleteAdmin mark

---
+++ src/components/Mark/index.tsx
@@ -0,0 +1,22 @@
+type MarkProps = {
+  target: string;
+  keyword?: string;
+  keywordSize?: string;
+  color?: string;
+};
+export const Mark = ({ target, keyword, color, keywordSize }: MarkProps) => {
+  if (!keyword) return <>{target}</>;
+  const stringSlices = target.split(keyword);
+  return (
+    <>
+      {stringSlices.map((str, index) => (
+        <span key={index}>
+          {str}
+          {index === stringSlices.length - 1 ? null : (
+            <span style={{ fontSize: keywordSize, color: color || '#448df7' }}>{keyword}</span>
+          )}
+        </span>
+      ))}
+    </>
+  );
+};
