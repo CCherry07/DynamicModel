70f7eca4f5cae3fa6001d5cd9aa07c69a0684005
2405693142@qq.com <2405693142@qq.com>
2022-05-31 11:32:38 +0800

    feat:  FooterToolbar

--- src/pages/BasicList/components/AfterTableLayout.tsx
+++ src/pages/BasicList/components/AfterTableLayout.tsx
@@ -2,12 +2,8 @@ import { Row, Col } from 'antd';
 export const AfterTableLayout = () => {
   return (
     <Row>
-      <Col xs={24} sm={12}>
-        ...
-      </Col>
-      <Col xs={24} sm={12} style={{ textAlign: 'right' }}>
-        tableToolbal
-      </Col>
+      <Col xs={24} sm={12} />
+      <Col xs={24} sm={12} style={{ textAlign: 'right' }} />
     </Row>
   );
 };
